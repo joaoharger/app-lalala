a3aa734a76390cc033a41adb72f707d10f549c71
João Harger <joao-harger@hotmail.com>
2018-05-11 17:13:48 -0300

    xxx

--- src/components/Quadro-projeto.vue
+++ src/components/Quadro-projeto.vue
@@ -1,190 +1,234 @@
 <template>
-<div class="row">
-
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-10">
-            <h2>Quadro de tarefas</h2>
-            <ol class="breadcrumb">
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li>
-                    <a>Projetos</a>
-                </li>
-                <li class="active">
-                    <strong>Projeto X</strong>
-                </li>
-            </ol>
+    <div class="row">
+        <div class="row wrapper border-bottom white-bg page-heading">
+            <div class="col-lg-10">
+                <h2>Quadro de tarefas</h2>
+                <ol class="breadcrumb">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a>Projetos</a>
+                    </li>
+                    <li class="active">
+                        <strong>Projeto X</strong>
+                    </li>
+                </ol>
+            </div>
         </div>
-    </div>
+        <div class="wrapper wrapper-content  animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <h3>Pendente</h3>
+                            <p class="small">
+                                <i class="fa fa-hand-o-up"></i> Arraste a tarefa entre as listas ou posições</p>
 
-    <div class="wrapper wrapper-content  animated fadeInRight">
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="ibox">
-                    <div class="ibox-content">
-                        <h3>Pendente</h3>
-                        <p class="small">
-                            <i class="fa fa-hand-o-up"></i> Arraste a tarefa entre as listas ou posições</p>
+                            <div class="input-group">
+                                <input v-model="nomeNovaTarefa" type="text" placeholder="Título da nova tarefa. " class="input input-sm form-control">
+                                <span class="input-group-btn">
+                                    <button id="addTarefa" type="button" class="btn btn-sm btn-white" data-toggle="modal" data-target="#criaTarefa">
+                                        <i class="fa fa-plus"></i> Adicionar nova terefa</button>
+                                </span>
+                            </div>
 
-                        <div class="input-group">
-                            <input type="text" placeholder="Título da nova tarefa. " class="input input-sm form-control">
-                            <span class="input-group-btn">
-                                <button type="button" class="btn btn-sm btn-white" data-toggle="modal" data-target="#criaTarefa">
-                                    <i class="fa fa-plus"></i> Adicionar nova terefa</button>
-                            </span>
+                            <ul v-for="tarefa in tarefasFiltradas" :key="tarefa._id" class="sortable-list connectList agile-list" id="todo">
+                                <li class="warning-element" id="task1" @click="abreTarefa(tarefa)">
+                                    <span>{{tarefa.nome}}</span>
+                                    <span> - {{tarefa.descricao}}</span>
+                                    <div class="agile-detail">
+                                        <a v-if="tarefa.tags[0] != null" class="pull-right btn btn-xs btn-white">{{tarefa.tags[0]}}</a>
+                                        <a v-if="tarefa.tags[1] != null" class="pull-right btn btn-xs btn-white">{{tarefa.tags[1]}}</a>
+                                        <a v-if="tarefa.tags[2] != null" class="pull-right btn btn-xs btn-white">{{tarefa.tags[2]}}</a>
+                                        <i class="fa fa-clock-o"></i>
+                                        <span v-if="tarefa.data_entrega != null ">{{tarefa.data_entrega}}</span>
+                                        <span v-else>Data não definida</span>
+                                        <span>| Custo efetivo: {{tarefa.custo_efetivo}} </span>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <h3>Executando</h3>
 
-                        <ul class="sortable-list connectList agile-list" id="todo">
-                            <li class="warning-element" id="task1">
-                                <span>Pintar Sala</span>
-                                <span> - Descrição</span>
-                                <div class="agile-detail">
-                                    <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
-                                    <a href="#" class="pull-right btn btn-xs btn-white">Tag2</a>
-                                    <i class="fa fa-clock-o"></i>
-                                    <span>12.10.2015</span>
-                                    <span>| Custo efetivo: R$xxx,xx </span>
-                                </div>
-                            </li>
-                        </ul>
+                            <ul class="sortable-list connectList agile-list" id="inprogress">
+                                <li class="success-element" id="task9">
+                                    Quisque venenatis ante in porta suscipit.
+                                    <div class="agile-detail">
+                                        <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                                        <i class="fa fa-clock-o"></i> 12.10.2015
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ibox">
+                        <div class="ibox-content">
+                            <h3>Finalizada</h3>
+
+                            <ul class="sortable-list connectList agile-list" id="completed">
+                                <li class="info-element" id="task16">
+                                    Sometimes by accident, sometimes on purpose (injected humour and the like).
+                                    <div class="agile-detail">
+                                        <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                                        <i class="fa fa-clock-o"></i> 16.11.2015
+                                    </div>
+                                </li>
+                            
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="ibox">
-                    <div class="ibox-content">
-                        <h3>Executando</h3>
 
-                        <ul class="sortable-list connectList agile-list" id="inprogress">
-                            <li class="success-element" id="task9">
-                                Quisque venenatis ante in porta suscipit.
-                                <div class="agile-detail">
-                                    <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
-                                    <i class="fa fa-clock-o"></i> 12.10.2015
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4>Serialised Output</h4>
+                    <p>Serializes the sortable's item id's into an array of string.</p>
+                    <div class="output p-m m white-bg"></div>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="ibox">
-                    <div class="ibox-content">
-                        <h3>Finalizada</h3>
-
-                        <ul class="sortable-list connectList agile-list" id="completed">
-                            <li class="info-element" id="task16">
-                                Sometimes by accident, sometimes on purpose (injected humour and the like).
-                                <div class="agile-detail">
-                                    <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
-                                    <i class="fa fa-clock-o"></i> 16.11.2015
-                                </div>
-                            </li>
-                           
-                        </ul>
+        </div>
+        <div class="modal inmodal" id="criaTarefa" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content animated bounceInRight">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span>
+                        </button>
+                        <i class="fa fa-cubes modal-icon"></i>
+                        <h4 class="modal-title">Nova tarefa -
+                            <span>{{this.nomeNovaTarefa}}</span>
+                        </h4>
+                    </div>
+                    <div class="modal-body text-left">
+                        <div class="form-group">
+                            <label>Observações</label>
+                            <input type="text" placeholder="Descreva qualquer informação relevante" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Custo estimado</label>
+                            <input type="number" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Custo efetivo</label>
+                            <input type="number" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Pagamento em dinheiro</label>
+                            <input type="number" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Condição de parcelamento</label>
+                            <input type="text" placeholder="Ex: 30 60 90" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Condição de parcelamento</label>
+                            <input type="text" placeholder="Ex: 500 2000 2000" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Pagamento em permuta</label>
+                            <input type="number" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Tags</label>
+                            <select data-placeholder="Escolha as tags" class="chosen-select form-control" multiple tabindex="4">
+                                <option value="aguardando-instalacao" class="bg-info">Aguardando instalação</option>
+                                <option value="aguardando-entrega">Aguardando etrega</option>
+                                <option value="programado">Programado</option>
+                                <option value="urgente">Urgente</option>
+                                <option value="aguardando-decisao">Aguardando Decisão</option>
+                                <option value="andamento">Em andamento</option>
+                                <option value="pgto-pendente">Pgto pendente</option>
+                                <option value="aguardando-orcamento">Aguardando orçamento</option>
+                                <option value="interrompido">Interrompido</option>
+                                <option value="concluido">Concluído</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Fornecedor</label>
+                            <input type="text" placeholder="Nome do fornecedor" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Data da entrega</label>
+                            <input type="date" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-white" data-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary">Salvar</button>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Serialised Output</h4>
-                <p>Serializes the sortable's item id's into an array of string.</p>
-                <div class="output p-m m white-bg"></div>
-            </div>
-        </div>
-
+    
     </div>
-    <div class="modal inmodal" id="criaTarefa" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content animated bounceInRight">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">
-                        <span aria-hidden="true">&times;</span>
-                        <span class="sr-only">Close</span>
-                    </button>
-                    <i class="fa fa-cubes modal-icon"></i>
-                    <h4 class="modal-title">Nova tarefa -
-                        <span>NOME DA TAREFA</span>
-                    </h4>
-                </div>
-                <div class="modal-body text-left">
-                    <div class="form-group">
-                        <label>Observações</label>
-                        <input type="text" placeholder="Descreva qualquer informação relevante" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Custo estimado</label>
-                        <input type="number" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Custo efetivo</label>
-                        <input type="number" class="form-control">
-                    </div>
-                    <div class="form-group col-lg-4">
-                        <label>Pagamento em dinheiro</label>
-                        <input type="number" class="form-control">
-                    </div>
-                    <div class="form-group col-lg-4">
-                        <label>Condição de parcelamento</label>
-                        <input type="text" placeholder="Ex: 30 60 90" class="form-control">
-                    </div>
-                    <div class="form-group col-lg-4">
-                        <label>Condição de parcelamento</label>
-                        <input type="text" placeholder="Ex: 500 2000 2000" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Pagamento em permuta</label>
-                        <input type="number" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Tags</label>
-                        <select data-placeholder="Escolha as tags" class="chosen-select form-control" multiple tabindex="4">
-                            <option value="aguardando-instalacao" class="bg-info">Aguardando instalação</option>
-                            <option value="aguardando-entrega">Aguardando etrega</option>
-                            <option value="programado">Programado</option>
-                            <option value="urgente">Urgente</option>
-                            <option value="aguardando-decisao">Aguardando Decisão</option>
-                            <option value="andamento">Em andamento</option>
-                            <option value="pgto-pendente">Pgto pendente</option>
-                            <option value="aguardando-orcamento">Aguardando orçamento</option>
-                            <option value="interrompido">Interrompido</option>
-                            <option value="concluido">Concluído</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Fornecedor</label>
-                        <input type="text" placeholder="Nome do fornecedor" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Data da entrega</label>
-                        <input type="date" class="form-control">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-white" data-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary">Salvar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 </template>
 
 <script>
+import lista_tarefas from "../listaTarefas";
 
-    import lista_Tarefas from '../listaTarefas'
+export default {
+  mounted() {
+    $(".chosen-select").chosen({ width: "100%" });
 
-    export default { 
-        mounted() {
-            $(".chosen-select").chosen({ width: "100%" })
-        },
-        
+    this.listarTarefas();
+
+    $("#todo, #inprogress, #completed")
+      .sortable({
+        connectWith: ".connectList",
+        update: function(event, ui) {
+          var todo = $("#todo").sortable("toArray");
+          var inprogress = $("#inprogress").sortable("toArray");
+          var completed = $("#completed").sortable("toArray");
+          $(".output").html(
+            "ToDo: " +
+              window.JSON.stringify(todo) +
+              "<br/>" +
+              "In Progress: " +
+              window.JSON.stringify(inprogress) +
+              "<br/>" +
+              "Completed: " +
+              window.JSON.stringify(completed)
+          );
+        }
+      })
+      .disableSelection();
+  },
+  data: () => ({
+    tarefas: [],
+    termoPesquisa: "",
+    nomeNovaTarefa: "",
+    loading: true
+  }),
+  computed: {
+    tarefasFiltradas() {
+      return this.tarefas.filter(p => p.nome.includes(this.termoPesquisa));
     }
-
+  },
+  methods: {
+    listarTarefas() {
+      fetch("http://www.pudim.com.br/", { mode: "no-cors" }).then(response => {
+        this.tarefas = lista_tarefas.map(p => ({
+          ...p
+        }));
+        loading: false;
+      });
+    },
+    abreTarefa(index) {
+      console.log(index);
+      $("#abreTarefa").click();
+    }
+  }
+};
 </script>
 
 <style scoped>
--- src/listaTarefas.js
+++ src/listaTarefas.js
@@ -1,99 +1,31 @@
 export default [
     {
-        "todo": 
-        [
-            {
-                "_id": "ddowji1210980",
-                "nome": "Pintar sala",
-                "observacoes": "Cor Azul xyz",
-                "custo-estimado": 1500,
-                "custo-efetivo": 1450,
-                "pagamento-dinheiro": 945,
-                "condicao-pgto": "1 +1",
-                "condicao-parcelamento": "450 500",
-                "pagamento-permuta": 500,
-                "tags": ["Urgente", "Aguardando orçamento"],
-                "fornecedor": "Casa dos pintores",
-                "data-entrega": null,
-            },
-            {
-                "_id": "ddowji1210982",
-                "nome": "Retirando Cozinha",
-                "observacoes": "Cor Azul xyz",
-                "custo-estimado": 1000,
-                "custo-efetivo": 1000,
-                "pagamento-dinheiro": 800,
-                "condicao-pgto": "1 +1",
-                "condicao-parcelamento": "400 400",
-                "pagamento-permuta": 200,
-                "tags": ["Urgente", "Aguardando orçamento"],
-                "fornecedor": "Casa dos pintores",
-                "data-entrega": null,
-            }
-        ],
-        "doing": 
-        [
-            {
-                "_id": "ddowji1210911",
-                "nome": "Retirando moveis",
-                "observacoes": "Caçamba disponivel até dia XXX",
-                "custo-estimado": 800,
-                "custo-efetivo": 800,
-                "pagamento-dinheiro": 800,
-                "condicao-pgto": "1",
-                "condicao-parcelamento": "",
-                "pagamento-permuta": null,
-                "tags": ["Programado"],
-                "fornecedor": "Interno - Cechinel",
-                "data-entrega": "2018.05.28"
-            },
-            {
-                "_id": "ddowji1210912",
-                "nome": "Projeto arquiteto",
-                "observacoes": "padrão moderno",
-                "custo-estimado": 20.000,
-                "custo-efetivo": 20.000,
-                "pagamento-dinheiro": 10.000,
-                "condicao-pgto": "1 30 60",
-                "condicao-parcelamento": "2000 4000 4000",
-                "pagamento-permuta": 10.000,
-                "tags": ["Urgente"],
-                "fornecedor": "Casa dos pintores",
-                "data-entrega": "2018.03.03",
-            }
-        ],
-        "did": 
-        [
-            {
-                "_id": "ddowji1210922",
-                "nome": "Revisao de impostos",
-                "observacoes": "nada pendente",
-                "custo-estimado": 0,
-                "custo-efetivo": 1500,
-                "pagamento-dinheiro": 1500,
-                "condicao-pgto": "1",
-                "condicao-parcelamento": "",
-                "pagamento-permuta": null,
-                "tags": [""],
-                "fornecedor": "Prefeitura",
-                "data-entrega": "2018.03.25"
-            },
-            {
-                "_id": "ddowji1210923",
-                "nome": "Quebrar parede sala",
-                "observacoes": "",
-                "custo-estimado": 500,
-                "custo-efetivo": 500,
-                "pagamento-dinheiro": 500,
-                "condicao-pgto": "",
-                "condicao-parcelamento": "",
-                "pagamento-permuta": null,
-                "tags": ["Concluído"],
-                "fornecedor": "Interno - Cechinel",
-                "data-entrega": "2018.03.03",
-            }
-        ]  
-
+        
+        "_id": "ddowji1210980",
+        "nome": "Pintar sala",
+        "descricao": "Cor Azul xyz",
+        "custo_estimado": 1500,
+        "custo_efetivo": 1450,
+        "pagamento_dinheiro": 945,
+        "condicao_pgto": "1 +1",
+        "condicao_parcelamento": "450 500",
+        "pagamento_permuta": 500,
+        "tags": ["Urgente"],
+        "fornecedor": "Casa dos pintores",
+        "data_entrega": null,
+    },
+    {
+        "_id": "ddowji1210982",
+        "nome": "Retirando Cozinha",
+        "descricao": "Cor Azul xyz",
+        "custo_estimado": 1000,
+        "custo_efetivo": 1000,
+        "pagamento_dinheiro": 800,
+        "condicao_pgto": "1 +1",
+        "condicao_parcelamento": "400 400",
+        "pagamento_permuta": 200,
+        "tags": ["Urgente", "Aguardando orçamento", "Atrasado"],
+        "fornecedor": "Casa dos pintores",
+        "data_entrega": "2018.05.10",
     }
-
 ]
